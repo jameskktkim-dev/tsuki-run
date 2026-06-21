9430380761a1635bdfbda17d3c6ca16e195362d3
james <james.kim.etc@gmail.com>
2026-06-21 12:34:58 -0700

    Initial Tsuki Run setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Tsuki Run</h1>
+
+      <div>
+        Monthly Stats
+      </div>
+
+      <div>
+        Calendar Goes Here
+      </div>
+
+      <div>
+        Weekly Stats
+      </div>
+    </div>
+  );
+}
